60b6c8a111585eb386e02a2c53ec8dcce316b538
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-13 22:17:26 +0400

    formatting adjustment

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
-import {RouterModule} from '@angular/router';
-import {ReactiveFormsModule} from '@angular/forms';
-import {ButtonsModule, CardsModule, InputsModule, InputUtilitiesModule, WavesModule} from 'angular-bootstrap-md';
 import { RegisterComponent } from './components/register/register.component';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonsModule, CardsModule, InputsModule, InputUtilitiesModule, WavesModule } from 'angular-bootstrap-md';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
@@ -16,7 +17,9 @@ import { RegisterComponent } from './components/register/register.component';
     InputUtilitiesModule,
     ButtonsModule,
     WavesModule,
-    InputsModule
-  ]
+    InputsModule,
+  ],
+  exports: [RegisterComponent],
+  providers: [AuthService]
 })
 export class AuthModule { }
