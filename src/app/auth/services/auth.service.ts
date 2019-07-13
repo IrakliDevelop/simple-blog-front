cdcde17b144a507c32adc53dc3c555fa67ea7205
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-13 20:40:07 +0400

    create auth service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private readonly URL = environment.ApiURL;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(`${this.URL}/api/auth`, { username, password });
+  }
 }
