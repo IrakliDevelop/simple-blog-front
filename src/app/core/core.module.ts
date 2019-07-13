bf1bb3a5a7668143271e36ad8e0de7029d549408
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-13 21:20:49 +0400

    create main component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, MainComponent],
   imports: [
     CommonModule
   ]
