179e66aaa83366deb2412b55883767bbe3f1bb8d
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-16 22:05:51 +0400

    install and initialize @auth0/angular-jwt package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
+import { JwtModule } from '@auth0/angular-jwt';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 
+
+const URL = environment.ApiURL;
 
 @NgModule({
   declarations: [
@@ -16,6 +19,14 @@ import { AuthModule } from './auth/auth.module';
     HttpClientModule,
     AppRoutingModule,
     AuthModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => {
+          return localStorage.getItem('access_token');
+        },
+        whitelistedDomains: [URL]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
