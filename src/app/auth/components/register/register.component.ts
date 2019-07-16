b62e64e4769419d55a9f6a83196570ba5c7f9cc8
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-16 22:06:28 +0400

    implement unsubscribe

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 import { AuthService } from '../../services/auth.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
   registerForm: FormGroup;
   // TODO: implement this
   error$: Observable<string | null>;
+  unsubscribe$: Subject<void>;
 
   constructor(
     public fb: FormBuilder,
@@ -19,7 +22,8 @@ export class RegisterComponent implements OnInit {
   ) {
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.unsubscribe$ = new Subject<void>();
     this.registerForm = this.fb.group({
       username: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)]),
@@ -31,26 +35,30 @@ export class RegisterComponent implements OnInit {
   get username(): AbstractControl {
     return this.registerForm.get('username');
   }
-
   get password(): AbstractControl {
     return this.registerForm.get('password');
   }
-
   get firstname(): AbstractControl {
     return this.registerForm.get('firstname');
   }
-
   get lastname(): AbstractControl {
     return this.registerForm.get('lastname');
   }
 
-  onRegister() {
+  onRegister(): void {
     const username = this.registerForm.value.username;
     const password = this.registerForm.value.password;
     const firstname = this.registerForm.value.firstname;
     const lastname = this.registerForm.value.lastname;
     // TODO: pipe and subscribe
-    this.authService.register(username, password, firstname, lastname);
+    this.authService.register(username, password, firstname, lastname)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(data => console.log(data));
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
