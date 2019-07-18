9498ae1497367bff0ceff341f353aab58251dbd4
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-07-18 23:25:27 +0400

    implement token related services in auth service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
+import {Injectable} from '@angular/core';
+import {environment} from '../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {JwtHelperService} from '@auth0/angular-jwt';
+
+const helper = new JwtHelperService();
+
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +23,22 @@ export class AuthService {
   register(username, password, firstname, lastname): Observable<any> {
     return this.http.post(`${this.URL}/api/auth/register`, {username, password, firstname, lastname});
   }
+  isAuthenticated(): boolean {
+    return !this.isExpired(this.getToken());
+  }
   setToken(token: string): void {
     localStorage.setItem('access_token', token);
+  }
+  getToken(): string {
+    return localStorage.getItem('access_token');
+  }
+  decodeToken(token: string): any {
+    return helper.decodeToken(token);
+  }
+  getTokenExpirationDate(token: string): any {
+    return helper.getTokenExpirationDate(token);
+  }
+  isExpired(token: string): boolean {
+    return helper.isTokenExpired(token);
   }
 }
